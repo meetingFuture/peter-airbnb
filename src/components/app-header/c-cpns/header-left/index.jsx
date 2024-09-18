e17f07931f639b7092240871825673618faebbd8
Peter Sun <hailong.sun1982@gmail.com>
2024-09-18 11:30:53 -0600

    feat: add svg logo

--- src/components/app-header/c-cpns/header-left/index.jsx
+++ src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,6 +1,12 @@
 import React, { memo } from "react";
 import { LeftWrapper } from "./style";
+import IconLogo from "../../../../assets/svg/icon_logo";
 
 export default memo(function index() {
-  return <LeftWrapper>header-left</LeftWrapper>;
+  return (
+    <LeftWrapper>
+      <IconLogo />
+      header-left
+    </LeftWrapper>
+  );
 });
